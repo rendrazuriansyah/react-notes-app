92a7bbd218ac7dd44e31f8c2a4e3ca30942f6aa0
rendrazuriansyah <rendra.zurd04@gmail.com>
2024-08-14 02:47:42 +0700

    feat(app): create initial structure of App component feat(notes): add note input handling with useState feat(notes): implement note submission functionality feat(notes): add delete functionality for notes style(app): update CSS classes for component styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [notes, setNotes] = useState([]);
+	const [inputNote, setInputNote] = useState("");
+
+	function handleInputChange(e) {
+		setInputNote(e.target.value);
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		if (inputNote.trim !== "") {
+			const newNote = {
+				id: Date.now(),
+				text: inputNote,
+			};
+			setNotes([...notes, newNote]);
+			setInputNote("");
+		}
+	}
+
+	function handleDeleteNote(id) {
+		const newNotes = notes.filter((note) => note.id !== id);
+		setNotes(newNotes);
+	}
+
+	return (
+		<>
+			<div
+				className="App"
+				onSubmit={handleSubmit}
+			>
+				<h1>Notes</h1>
+				<form className="note-input">
+					<input
+						type="text"
+						placeholder="Add a note"
+						required
+						autoFocus
+						minLength={5}
+						maxLength={255}
+						autoComplete="off"
+						value={inputNote}
+						onChange={handleInputChange}
+					/>
+					<button>Add</button>
+				</form>
+				<ul className="note-list">
+					{notes.map((note) => (
+						<li key={note.id}>
+							<span>{note.text}</span>
+							<button
+								onClick={handleDeleteNote.bind(null, note.id)}
+							>
+								Delete
+							</button>
+						</li>
+					))}
+				</ul>
+			</div>
+		</>
+	);
 }
 
 export default App;
